c7cc5bbaed3fb351768d5b0977c309cab0ee739b
brokiem <milyhqvideo@gmail.com>
2021-03-18 01:22:01 +0800

    update

--- src/brokiem/customentity/Main.php
+++ src/brokiem/customentity/Main.php
@@ -12,6 +12,7 @@ class Main extends PluginBase
     public function onEnable(): void
     {
         $entityName = "villager"; // this is used when using the SimpleNPC spawn command
-        SimpleNPC::registerEntity(CustomVillager::class, $entityName);
+        SimpleNPC::registerEntity(CustomVillager::class, $entityName); // register the entity to SimpleNPC
+        // NOTE: Use SimpleNPC::registerEntity();! NOT Entity::registerEntity();
     }
 }
