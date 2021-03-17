67432fa9c4638d3ac941bc0967a20b3e10310ae4
brokiem <milyhqvideo@gmail.com>
2021-03-18 00:30:46 +0800

    Don't forget to add the network id of the entity

--- src/brokiem/customentity/CustomVillager.php
+++ src/brokiem/customentity/CustomVillager.php
@@ -1,11 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
 namespace brokiem\customentity;
 
 use brokiem\snpc\entity\BaseNPC;
+use pocketmine\entity\Entity;
 
 class CustomVillager extends BaseNPC /* Make sure your entity class extends to \brokiem\snpc\entity\BaseNPC */
 {
+    public const NETWORK_ID = Entity::VILLAGER; // Don't forget to add the network id of the entity
+
     protected $gravity = 0.1; // u can edit anything here
 }
